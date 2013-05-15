c1925cfb2d185fc336af388679a469f604be8a93
Alexandre Strzelewicz <strzelewicz.alexandre@gmail.com>
2013-05-15 18:47:17 +0800

    test base + fix package.json

--- test/test.js
+++ test/test.js
@@ -1,42 +1,70 @@
 var should = require("should"),
-    request = require("supertest"),
+    supertest = require("supertest"),
     angularBridge = require("../index"),
     express = require("express"),
+    request = require('request'),
     mongoose = require("mongoose"),
+    http = require('http'),
     schema = require("./schema");
+
+const URL = 'http://localhost:4000/';
+const RESOURCE = 'pizzasto';
 
 describe('Angular-bridge', function(){
     var bridge;
     var app;
-
-    beforeEach(function(done){
+    var server;
+    
+    before(function(done){
         app = express();
         app.use(express.bodyParser());
 
         bridge = new angularBridge(app, null);
-        bridge.addResource('pizzas', schema.Pizza);
-        done();
+        bridge.addResource(RESOURCE, schema.Pizza);
+
+	server = http.createServer(app);
+
+	server.listen(4000, function(){
+	    done();
+	});
+        
     });
 
-    it('GET /pizzas', function(done){
-        request(app)
-          .get('/pizzas')
-          .expect(200, done);
+    after(function() {
+	server.close();	
     });
 
-    it('POST /pizzas', function(done){
-        request(app)
-          .post('/pizzas')
-          .send({author: "Author", color: "black", size: 4, password: "secret"})
-          .expect(200, done);
+    describe('Right status', function() {
+	it('GET /pizzas', function(done){
+            supertest(app)
+		.get('/' + RESOURCE)
+		.expect(200, done);
+	});
+	
+	it('POST /pizzas', function(done){
+            supertest(app)
+		.post('/' + RESOURCE)
+		.send({author: "Author", color: "black", size: 4, password: "secret"})
+		.expect(200, done);
+	});
+	
+	it('POST /pizza - invalid data', function(done){
+            supertest(app)
+		.post('/' + RESOURCE)
+		.send({author: "Author", color: "black", size: "four", password: "secret"})
+		.expect(400, done);
+	});
     });
 
-     it('POST /pizza - invalid data', function(done){
-        request(app)
-          .post('/pizzas')
-          .send({author: "Author", color: "black", size: "four", password: "secret"})
-          .expect(400, done);
-     });
+    describe('Data exchange', function() {
+	
+	it.skip('should get nothing', function(done) {
+	    request(URL + RESOURCE, function(err, req, body) {
+		
+	    });
+	});
+
+    });
 });
 
 
